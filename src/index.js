6770f58ca30e6e63a5e5477e5c90a95f8d9b1b8b
Redzuan <rabdulr@icloud.com>
2020-09-24 16:22:40 -0700

    initial tweaks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+    return(
+        <div>
+            <h1>GitCheck FSA</h1>
+            <a href="https://github.com/login/oauth/authorize?client_id=2d9066f1cc065f4ad732&redirect_uri=http://localhost:3000/callback">Login with gitHub</a>
+        </div>
+    )
+}
+
 const root = document.querySelector('#root');
 
-ReactDOM.render(<hr />, root);
+ReactDOM.render(<App />, root);
