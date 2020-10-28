7b1d3a9b5d00f7473f70d69d0dd8aa62e69f904f
Redzuan <rabdulr@icloud.com>
2020-10-28 14:48:45 -0700

    tiny changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './app';
 import {HashRouter} from 'react-router-dom';
 
 const root = document.querySelector('#root');
