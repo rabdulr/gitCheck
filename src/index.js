88d77f5349d5d10b864aa6b2312dd35d973095cc
Redzuan <rabdulr@icloud.com>
2020-10-28 22:14:56 -0700

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, root)
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, root)
